c22848b799b35217707564864c3c2e53a0ff4bcb
BeneZ <bezraat@gmail.com>
2021-06-09 16:40:42 +0200

    findRealese

--- movie-app/src/main/java/movie/MovieService.java
+++ movie-app/src/main/java/movie/MovieService.java
@@ -1,9 +1,15 @@
 package movie;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class MovieService {
 
     private List<Movie> movies = new ArrayList<>();
+
+    public Optional<Movie> findNewestByRelease() {
+        return movies.stream().max(Comparator.comparing(Movie::getReleaseDate));
+    }
 }
