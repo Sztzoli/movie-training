193716712d706141b7dea20d04b4bbc6495920a4
Radacs-Bence <bence.radacs@gmail.com>
2021-06-09 16:47:35 +0200

    findByFragment method

--- movie-app/src/main/java/movie/MovieService.java
+++ movie-app/src/main/java/movie/MovieService.java
@@ -2,8 +2,18 @@ package movie;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class MovieService {
 
     private List<Movie> movies = new ArrayList<>();
+
+    private Movie findByFragment(String fragment){
+        Optional<Movie> search = movies.stream().filter(movie -> movie.getName().contains(fragment)).findFirst();
+        if (search.isEmpty()){
+            throw new IllegalArgumentException("Movie not found");
+        }
+        return search.get();
+    }
+
 }
